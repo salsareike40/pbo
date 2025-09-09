b06000da6c16fc4c1de1d85106a2ab1800454489
F1D02310136_Salsa Reike Maharani <salsareike40@gmail.com>
2025-09-09 10:29:27 +0800

    Delete Kalkulator directory

--- Kalkulator/Kalkulator.java
+++
@@ -1,9 +0,0 @@
-public class Kalkulator {
-    public int jumlahkan(int bilanganPertama, int bilanganKedua) {
-        return bilanganPertama + bilanganKedua;
-    }
-
-    public int kalikan(int bilanganPertama, int bilanganKedua) {
-        return bilanganPertama * bilanganKedua;
-    }
-}
